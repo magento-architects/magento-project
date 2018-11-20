795aa3fa3a95245138cc0fd5f08df448ac47d108
Anton Kril <anton.kril@gmail.com>
2018-11-19 21:27:57 -0600

    Cleanup & removed registration.php

--- pub/index.php
+++ pub/index.php
@@ -5,13 +5,9 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-use \Magento\Webapi\Model\Config\Converter;
-use Magento\Framework\Webapi\Rest\Request as RestRequest;
-use \Magento\Framework\Webapi\Rest\Response\FieldsFilter;
-use \Magento\Framework\Config\ConfigOptionsListConstants;
 define('BP', realpath(__DIR__ . '/..'));
 include BP . "/vendor/autoload.php";
-$objectManager = createObjectManager('webapi_rest', $_SERVER);
+$objectManager = \Magento\Framework\App\ObjectManagerFactory::create('webapi_rest', $_SERVER);
 /** @var \Magento\Framework\Webapi\Rest\Request $request */
 $request = $objectManager->get(\Magento\Framework\Webapi\Rest\Request::class);
 /** @var \Magento\Webapi\Controller\Rest $front */
